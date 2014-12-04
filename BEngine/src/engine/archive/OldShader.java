dd49e0bf830cd16c82da51cded7d94e6322f547b
blockout22 <kie1k93@gmail.com>
2014-12-04 07:17:13 +0000

    fixed compile error

--- BEngine/src/engine/archive/OldShader.java
+++ BEngine/src/engine/archive/OldShader.java
@@ -1,102 +1,103 @@
 package engine.archive;
-//package engine;
-//
-//import java.nio.FloatBuffer;
-//
-//import org.lwjgl.BufferUtils;
-//import org.lwjgl.opengl.GL11;
-//import org.lwjgl.opengl.GL20;
-//import org.lwjgl.util.vector.Matrix4f;
-//import org.lwjgl.util.vector.Vector3f;
-//
-//import engine.camera.Camera2;
-//
-//public class OldShader {
-//	
-//	private int programID;
-//	private int vertexID;
-//	private int fragmentID;
-//	
-//	private FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
-//	
-//	/**
-//	 * 
-//	 * @param vertexShader
-//	 * @param fragmentShader
-//	 */
-//	public OldShader(String vertexShader, String fragmentShader)
-//	{
-//		vertexID = loadShader(vertexShader, GL20.GL_VERTEX_SHADER);
-//		fragmentID = loadShader(fragmentShader, GL20.GL_FRAGMENT_SHADER);
-//		
-//		createProgram();
-//	}
-//	
-//	private void createProgram() {
-//		programID = GL20.glCreateProgram();
-//		GL20.glAttachShader(programID, vertexID);
-//		GL20.glAttachShader(programID, fragmentID);
-//	}
-//	
-//	public void bindAttribLocation(int attrib, String name)
-//	{
-//		GL20.glBindAttribLocation(programID, attrib, name);
-//	}
-//	
-//	/**
-//	 * call this after bindAttribLocation();
-//	 */
-//	public void linkAndValidate()
-//	{
-//		GL20.glLinkProgram(programID);
-//		GL20.glValidateProgram(programID);
-////		GL20.glUseProgram(programID);
-//	}
-//	
-//	public int getUniformLocation(String uniformName)
-//	{
-//		return GL20.glGetUniformLocation(programID, uniformName);
-//	}
-//	
-//	public void bind()
-//	{
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
+
+import engine.Storage;
+import engine.Util;
+import engine.entity.Entity;
+
+public class OldShader {
+	
+	private int programID;
+	private int vertexID;
+	private int fragmentID;
+	
+	private FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
+	
+	/**
+	 * 
+	 * @param vertexShader
+	 * @param fragmentShader
+	 */
+	public OldShader(String vertexShader, String fragmentShader)
+	{
+		vertexID = loadShader(vertexShader, GL20.GL_VERTEX_SHADER);
+		fragmentID = loadShader(fragmentShader, GL20.GL_FRAGMENT_SHADER);
+		
+		createProgram();
+	}
+	
+	private void createProgram() {
+		programID = GL20.glCreateProgram();
+		GL20.glAttachShader(programID, vertexID);
+		GL20.glAttachShader(programID, fragmentID);
+	}
+	
+	public void bindAttribLocation(int attrib, String name)
+	{
+		GL20.glBindAttribLocation(programID, attrib, name);
+	}
+	
+	/**
+	 * call this after bindAttribLocation();
+	 */
+	public void linkAndValidate()
+	{
+		GL20.glLinkProgram(programID);
+		GL20.glValidateProgram(programID);
 //		GL20.glUseProgram(programID);
-//	}
-//	
-//	public void unbind()
-//	{
-//		GL20.glUseProgram(0);
-//	}
-//	
-//	public void getUniform(int value, Matrix4f matrix)
-//	{
-//		matrix.store(matrixBuffer);
-//		
-//		matrixBuffer.flip();
-//		GL20.glUniformMatrix4(value, false, matrixBuffer);
-//	}
-//
-//	private int loadShader(String fileName, int type)
-//	{
-//		StringBuilder shaderSource = Util.loadFile(fileName);
-//		
-//		int shaderID = GL20.glCreateShader(type);
-//		GL20.glShaderSource(shaderID, shaderSource);
-//		GL20.glCompileShader(shaderID);
-//		
-//		if(GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
-//		{
-//			System.err.println(GL20.glGetShaderInfoLog(shaderID, 1000));
-//			Storage.cleanup();
-//			System.exit(-1);
-//		}
-//		
-//		return shaderID;
-//	}
-//
-//	public Matrix4f loadProjectionMatrix() {
-//		Matrix4f projectionMatrix = new Matrix4f();
-//
+	}
+	
+	public int getUniformLocation(String uniformName)
+	{
+		return GL20.glGetUniformLocation(programID, uniformName);
+	}
+	
+	public void bind()
+	{
+		GL20.glUseProgram(programID);
+	}
+	
+	public void unbind()
+	{
+		GL20.glUseProgram(0);
+	}
+	
+	public void getUniform(int value, Matrix4f matrix)
+	{
+		matrix.store(matrixBuffer);
+		
+		matrixBuffer.flip();
+		GL20.glUniformMatrix4(value, false, matrixBuffer);
+	}
+
+	private int loadShader(String fileName, int type)
+	{
+		StringBuilder shaderSource = Util.loadFile(fileName);
+		
+		int shaderID = GL20.glCreateShader(type);
+		GL20.glShaderSource(shaderID, shaderSource);
+		GL20.glCompileShader(shaderID);
+		
+		if(GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
+		{
+			System.err.println(GL20.glGetShaderInfoLog(shaderID, 1000));
+			Storage.cleanup();
+			System.exit(-1);
+		}
+		
+		return shaderID;
+	}
+
+	public Matrix4f loadProjectionMatrix() {
+		Matrix4f projectionMatrix = new Matrix4f();
+
 //		float y_scale = 1f / (float) Math.tan(Math.toRadians(Camera2.getFIELD_OF_VIEW() / 2f));
 //		float x_scale = y_scale / Camera2.getASPECT_RATIO();
 //		float frustum_length = Camera2.getZ_FAR() - Camera2.getZ_NEAR();
@@ -107,31 +108,31 @@ package engine.archive;
 //		projectionMatrix.m23 = -1;
 //		projectionMatrix.m32 = -((2 * Camera2.getZ_NEAR() * Camera2.getZ_FAR()) / frustum_length);
 //		projectionMatrix.m33 = 0;
-//		
-//		return projectionMatrix;
-//	}
-//
-//	public void loadTransformationMatrix(int location, Entity entity) {
-//		Matrix4f transformationMatrix = createTransformationMatrix(entity.getPosition(), entity.getRotationX(), entity.getRotationY(), entity.getRotationZ(), entity.getScale());
-//		getUniform(location, transformationMatrix);
-//	}
-//
-//	private Matrix4f createTransformationMatrix(Vector3f translation, float rx, float ry, float rz, float scale)
-//	{
-//		Matrix4f matrix = new Matrix4f();
-//		matrix.setIdentity();
-//		Matrix4f.translate(translation, matrix, matrix);
-//		Matrix4f.rotate((float)Math.toRadians(rx), new Vector3f(1, 0, 0), matrix, matrix);
-//		Matrix4f.rotate((float)Math.toRadians(ry), new Vector3f(0, 1, 0), matrix, matrix);
-//		Matrix4f.rotate((float)Math.toRadians(rz), new Vector3f(0, 0, 1), matrix, matrix);
-//		Matrix4f.scale(new Vector3f(scale, scale, scale), matrix, matrix);
-//		
-//		return matrix;
-//	}
-//
-//	public void loadProjectionMatrix(int projectionMatrix) {
-//		Matrix4f matrix = loadProjectionMatrix();
-//		getUniform(projectionMatrix, matrix);
-//	}
-//
-//}
+		
+		return projectionMatrix;
+	}
+
+	public void loadTransformationMatrix(int location, Entity entity) {
+		Matrix4f transformationMatrix = createTransformationMatrix(entity.getPosition(), entity.getRotationX(), entity.getRotationY(), entity.getRotationZ(), entity.getScale());
+		getUniform(location, transformationMatrix);
+	}
+
+	private Matrix4f createTransformationMatrix(Vector3f translation, float rx, float ry, float rz, float scale)
+	{
+		Matrix4f matrix = new Matrix4f();
+		matrix.setIdentity();
+		Matrix4f.translate(translation, matrix, matrix);
+		Matrix4f.rotate((float)Math.toRadians(rx), new Vector3f(1, 0, 0), matrix, matrix);
+		Matrix4f.rotate((float)Math.toRadians(ry), new Vector3f(0, 1, 0), matrix, matrix);
+		Matrix4f.rotate((float)Math.toRadians(rz), new Vector3f(0, 0, 1), matrix, matrix);
+		Matrix4f.scale(new Vector3f(scale, scale, scale), matrix, matrix);
+		
+		return matrix;
+	}
+
+	public void loadProjectionMatrix(int projectionMatrix) {
+		Matrix4f matrix = loadProjectionMatrix();
+		getUniform(projectionMatrix, matrix);
+	}
+
+}
--- BEngine/src/engine/archive/OldTexturedMesh.java
+++ BEngine/src/engine/archive/OldTexturedMesh.java
@@ -1,156 +1,160 @@
 package engine.archive;
-//package engine;
-//
-//import org.lwjgl.opengl.GL11;
-//import org.lwjgl.opengl.GL13;
-//import org.lwjgl.opengl.GL15;
-//import org.lwjgl.opengl.GL20;
-//import org.lwjgl.opengl.GL30;
-//import org.lwjgl.util.vector.Vector2f;
-//import org.lwjgl.util.vector.Vector3f;
-//import org.newdawn.slick.opengl.Texture;
-//import org.newdawn.slick.opengl.TextureLoader;
-//import org.newdawn.slick.util.ResourceLoader;
-//
-//public class OldTexturedMesh {
-//
-//	private int vaoID;
-//	private int vboID;
-//	private int vboTextureID;
-//	private int vboiID;
-//
-//	private int indicesSize;
-//
-//	private int TextureID = 1;
-//	
-//	public OldTexturedMesh(Shader shader, String textureFile) {
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+import engine.Storage;
+import engine.Util;
+import engine.entity.Entity;
+import engine.shader.Shader;
+
+public class OldTexturedMesh {
+
+	private int vaoID;
+	private int vboID;
+	private int vboTextureID;
+	private int vboiID;
+
+	private int indicesSize;
+
+	private int TextureID = 1;
+	
+	public OldTexturedMesh(Shader shader, String textureFile) {
 //		vaoID = Storage.addVAO();
 //		vboID = Storage.addVBO();
 //		vboTextureID = Storage.addVBO();
 //		vboiID = Storage.addVBO();
-//
-//		TextureID = loadTexture(textureFile);
-//		shader.bind();
+
+		TextureID = loadTexture(textureFile);
+		shader.bind();
 //		shader.loadProjectionMatrix(1);
-//		shader.unbind();
-//	}
-//
-//	public OldTexturedMesh(String textureFile) {
+		shader.unbind();
+	}
+
+	public OldTexturedMesh(String textureFile) {
 //		vaoID = Storage.addVAO();
 //		vboID = Storage.addVBO();
 //		vboTextureID = Storage.addVBO();
 //		vboiID = Storage.addVBO();
-//
-//		TextureID = loadTexture(textureFile);
-//	}
-//
-//	public void add(float[] vertices, float[] texCoords, int[] indices) {
-//		indicesSize = indices.length;
-//
-//		GL30.glBindVertexArray(vaoID);
-//
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
-//		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL15.GL_STATIC_DRAW);
-//		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-//
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTextureID);
-//		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(texCoords), GL15.GL_STATIC_DRAW);
-//		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-//
-//		GL30.glBindVertexArray(0);
-//
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-//		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL15.GL_STATIC_DRAW);
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-//	}
-//
-//	public void add(Vector3f[] vertices, Vector2f[] texCoords, int[] indices) {
-//		indicesSize = indices.length;
-//
-//		GL30.glBindVertexArray(vaoID);
-//
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
-//		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL15.GL_STATIC_DRAW);
-//		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-//
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTextureID);
-//		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(texCoords), GL15.GL_STATIC_DRAW);
-//		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
-//		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-//
-//		GL30.glBindVertexArray(0);
-//
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-//		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL15.GL_STATIC_DRAW);
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-//	}
-//	
-//	public void addEntity(Entity entity)
-//	{
-//		
-//	}
-//	
-//	public void drawEntity(Entity entity, Shader shader, int location)
-//	{
-//		GL30.glBindVertexArray(vaoID);
-//
-//		GL20.glEnableVertexAttribArray(0);
-//		GL20.glEnableVertexAttribArray(1);
-//		
+
+		TextureID = loadTexture(textureFile);
+	}
+
+	public void add(float[] vertices, float[] texCoords, int[] indices) {
+		indicesSize = indices.length;
+
+		GL30.glBindVertexArray(vaoID);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTextureID);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(texCoords), GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+
+		GL30.glBindVertexArray(0);
+
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL15.GL_STATIC_DRAW);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+	public void add(Vector3f[] vertices, Vector2f[] texCoords, int[] indices) {
+		indicesSize = indices.length;
+
+		GL30.glBindVertexArray(vaoID);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTextureID);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(texCoords), GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+
+		GL30.glBindVertexArray(0);
+
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL15.GL_STATIC_DRAW);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+	
+	public void addEntity(Entity entity)
+	{
+		
+	}
+	
+	public void drawEntity(Entity entity, Shader shader, int location)
+	{
+		GL30.glBindVertexArray(vaoID);
+
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+		
 //		shader.getUniform(location, entity.transformEntity(entity.getPosition(), entity.getRotationX(), entity.getRotationY(), entity.getRotationZ(), entity.getScale()));
-//
-//		GL13.glActiveTexture(GL13.GL_TEXTURE0);
-//		GL11.glBindTexture(GL11.GL_TEXTURE_2D, TextureID);
-//		
-////		shader.loadTransformationMatrix(entity);
-//
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-//		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesSize, GL11.GL_UNSIGNED_INT, 0);
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-//
-//		GL20.glDisableVertexAttribArray(0);
-//		GL20.glDisableVertexAttribArray(1);
-//
-//		GL30.glBindVertexArray(0);
-//	}
-//
-//	public void draw() {
-//		GL30.glBindVertexArray(vaoID);
-//
-//		GL20.glEnableVertexAttribArray(0);
-//		GL20.glEnableVertexAttribArray(1);
-//
-//		GL13.glActiveTexture(GL13.GL_TEXTURE0);
-//		GL11.glBindTexture(GL11.GL_TEXTURE_2D, TextureID);
-//
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-//		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesSize, GL11.GL_UNSIGNED_INT, 0);
-//		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-//
-//		GL20.glDisableVertexAttribArray(0);
-//		GL20.glDisableVertexAttribArray(1);
-//
-//		GL30.glBindVertexArray(0);
-//	}
-//
-//	private int loadTexture(String textureFile) {
-//		Texture texture = null;
-//		try {
-//			texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(textureFile));
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.err.println("Can't find texture: " + textureFile);
-//			System.out.println("Using Default Texture");
-//			return 0;
-//		}
-//
-//		TextureID = texture.getTextureID();
+
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, TextureID);
+		
+//		shader.loadTransformationMatrix(entity);
+
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesSize, GL11.GL_UNSIGNED_INT, 0);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+
+		GL30.glBindVertexArray(0);
+	}
+
+	public void draw() {
+		GL30.glBindVertexArray(vaoID);
+
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, TextureID);
+
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, indicesSize, GL11.GL_UNSIGNED_INT, 0);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+
+		GL30.glBindVertexArray(0);
+	}
+
+	private int loadTexture(String textureFile) {
+		Texture texture = null;
+		try {
+			texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(textureFile));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Can't find texture: " + textureFile);
+			System.out.println("Using Default Texture");
+			return 0;
+		}
+
+		TextureID = texture.getTextureID();
 //		Storage.addTexture(TextureID);
-//		System.out.println("TexturedMesh TextureID: " + TextureID);
-//		return TextureID;
-//	}
-//
-//}
+		System.out.println("TexturedMesh TextureID: " + TextureID);
+		return TextureID;
+	}
+
+}
